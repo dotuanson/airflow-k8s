8cb376c080578ea5ce3266fde5db7cb0a6c06984
dotuanson <dotuansondn@gmail.com>
2024-06-13 15:42:37 +0700

    feat: test with postgres

---
+++ dags/test_with_postgres_operator.py
@@ -0,0 +1,29 @@
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+default_args = {
+    'owner': "sondt",
+    'retries': 5,
+    'retry_delay': pendulum.duration(minutes=5),
+}
+
+with DAG(
+    dag_id="test_dag_postgres_operator_v01",
+    default_args=default_args,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule_interval='0 0 * * *',
+) as dag:
+    task1 = PostgresOperator(
+        task_id='create_postgres_table',
+        postgres_conn_id='postgres_conn',
+        sql="""
+            create table if not exists dag_runs (
+                dt data,
+                dag_id chracter varying,
+                primary key (dt, dag_id)
+            )
+        """
+    )
+    task1
